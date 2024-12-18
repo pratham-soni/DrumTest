4b2677e84b381c68dd0f785fb731d98de8d60c12
pratham-soni <spratham304@gmail.com>
2024-12-18 19:14:50 +0530

    added event listener test-1

---
+++ index.js
@@ -0,0 +1,10 @@
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+for (var i=0; i<numberOfButtons; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        alert("I got clicked");
+        
+    });
+}
+
