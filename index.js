98bc819d0dc92107e535f4f3f6850244f9e3abbf
pratham-soni <spratham304@gmail.com>
2024-12-19 21:45:46 +0530

    added sound to key press events

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
 var numberOfButtons = document.querySelectorAll(".drum").length;
 
+function audPath (char) {  
+    switch (char) {
+        case 'w':
+            return "tom-1";
+        case 'a':
+            return "tom-2";
+        case 's':
+            return "tom-3";
+        case 'd':
+            return "tom-4";
+        case 'j':
+            return "crash";
+        case 'k':
+            return "kick-bass";
+        case 'l':
+            return "snare";
+        default:
+            return;
+    }
+}
+
 for (var i=0; i<numberOfButtons; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        // var audio = new Audio("./sounds/crash.mp3");
-        // audio.play();
 
         var buttonPressed = this.innerHTML;
-        var audioPath = '';
-        switch (buttonPressed) {
-            case 'w':
-                audioPath = "tom-1";
-                break;
-            case 'a':
-                audioPath = "tom-2";
-                break;
-            case 's':
-                audioPath = "tom-3";
-                break;
-            case 'd':
-                audioPath = "tom-4";
-                break;
-            case 'j':
-                audioPath = "crash";
-                break;
-            case 'k':
-                audioPath = "kick-bass";
-                break;
-            case 'l':
-                audioPath = "snare";
-                break;
-            default:
-                break;
-        }
+        var audioPath = audPath(buttonPressed);
+        
         audioPath = "./sounds/"+audioPath+".mp3";
-        console.log(audioPath);
         var audio = new Audio(audioPath);
         audio.play();
-
     });
 }
+
+document.addEventListener("keydown", function (event) {
+    var keyPressed = event.key;
+    var audioPath = audPath(keyPressed);
+    
+    audioPath = "./sounds/"+audioPath+".mp3";
+    var audio = new Audio(audioPath);
+    audio.play();
+
+})
